ae9a11924b705419bf31b71d0d60b239d25f758a
Themba-23 <snedla23@gmail.com>
2025-03-26 16:19:41 +0200

    Create python.py
    python assignment week 1

---
+++ python.py
@@ -0,0 +1,11 @@
+if operation =="+":
+results=num1+num2
+print(f"(num1)+(num2)=(result)")
+elif operation=="-":
+result = num1-num2
+print(f"(num1)-(num2) =(result)")
+elif operation=="*":
+result=num1*num2
+print(f"(num1)*(num2)=(result)")
+elif operation== "/":
+print(f"(num1)/(num2)=(result)")
