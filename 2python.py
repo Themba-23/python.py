1a2100f40a2955ce4987d85600eac305017ef5c9
Themba-23 <snedla23@gmail.com>
2025-03-26 16:40:39 +0200

    2python.py
    week 2 assignment

---
+++ 2python.py
@@ -0,0 +1,13 @@
+my_list=[]
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(1, 15)
+my_list.extend([50, 60, 70])
+my_list.remove(-1)
+my_list.sort()
+print("After sorting in ascending order:",my_list)
+index_of_30 = my_list.index(30)
+print("Index of value 30:", index_of_30)
+print("Final list:", my_list)
